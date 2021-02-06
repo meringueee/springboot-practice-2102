63f9e5503a9e1f0cdc7b6b97a04881f0db619ad6
Donghoon Kang <meringueee@naver.com>
2021-02-06 23:35:27 +0900

    hello controller 등록

---
+++ src/main/java/com/dingkang/spring/springboot/application.java
@@ -0,0 +1,12 @@
+package com.dingkang.spring.springboot;
+
+import javafx.application.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
